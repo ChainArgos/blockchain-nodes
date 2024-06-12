76337af383ae0c6638572be50b8994d4be89cbfc
Alexey Zhokhov <alexey@zhokhov.com>
2024-06-12 18:08:36 +0200

    Restart bitcoin script.

---
+++ restart-bitcoin-core.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+ABSOLUTE_PATH=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+cd "${ABSOLUTE_PATH}" || exit
+
+set -e
+
+docker compose pull bitcoin-core
+docker compose down bitcoin-core
+docker compose up -d bitcoin-core
+docker logs -f bitcoin-core
