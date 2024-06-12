88c7e4c1ba5eaf3b6a65c1aa3217a398c44bd26e
Alexey Zhokhov <alexey@zhokhov.com>
2024-06-13 00:05:44 +0200

    bsc fix

--- docker-bsc-geth/docker-entrypoint.sh
+++ docker-bsc-geth/docker-entrypoint.sh
@@ -4,7 +4,7 @@ set -eou pipefail
 if [ ! -d "/data/geth" ]; then
   echo "Init genesis with Hash-Base Storage Scheme by default."
 
-  /usr/local/bin/geth --db.engine=pebble --datadir=/data init /genesis.json
+  /usr/local/bin/geth --db.engine=pebble --datadir=/data/geth init /genesis.json
 fi
 
 echo "Initialization completed successfully"
