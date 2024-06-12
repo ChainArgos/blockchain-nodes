00d806de6bcb5d68d54a3675eb5422611267b705
Alexey Zhokhov <alexey@zhokhov.com>
2024-06-12 18:07:18 +0200

    Fixed data path for bitcoin.

--- docker-bitcoin-core/docker-entrypoint.sh
+++ docker-bitcoin-core/docker-entrypoint.sh
@@ -6,6 +6,8 @@ rpcuser=${RPCUSER:-chainargos}
 rpcpassword=${RPCPASSWORD:-`dd if=/dev/urandom bs=33 count=1 2>/dev/null | base64`}
 EOF
 
+mkdir -p /data/bitcoin-core
+
 echo "Initialization completed successfully"
 
 exec "$@"
