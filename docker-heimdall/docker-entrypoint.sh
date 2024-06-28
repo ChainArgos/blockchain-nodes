ff91fe5301900ba414c7fd8a005c46d4ddc2f900
Alexey Zhokhov <alexey@zhokhov.com>
2024-06-28 13:18:13 +0200

    Custom config for heimdall.

--- docker-heimdall/docker-entrypoint.sh
+++ docker-heimdall/docker-entrypoint.sh
@@ -4,7 +4,12 @@ set -eou pipefail
 if [ ! -d "/data/heimdalld" ]; then
   heimdalld init --chain=mainnet --home=/data/heimdalld --home-client=/data/heimdallcli
 
-  sed -i 's|^seeds =.*|seeds = "1500161dd491b67fb1ac81868952be49e2509c9f@52.78.36.216:26656,dd4a3f1750af5765266231b9d8ac764599921736@3.36.224.80:26656,8ea4f592ad6cc38d7532aff418d1fb97052463af@34.240.245.39:26656,e772e1fb8c3492a9570a377a5eafdb1dc53cd778@54.194.245.5:26656,6726b826df45ac8e9afb4bdb2469c7771bd797f1@52.209.21.164:26656"|g' /data/heimdalld/config/config.toml
+  # override with our config
+  cp /config.toml.sample /data/heimdalld/config/config.toml
+
+  moniker=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 12; echo)
+
+  sed -i -e "s/NODE_NAME/$moniker/g" /data/heimdalld/config/config.toml
 fi
 
 echo "Initialization completed successfully"
--- docker-heimdall/start.sh
+++ docker-heimdall/start.sh
@@ -5,5 +5,4 @@ exec heimdalld start \
   --chain=mainnet \
   --home=/data/heimdalld \
   --home-client=/data/heimdallcli \
-  --eth_rpc_url=http://ethereum-geth:8545 \
-  --log_level=*:info
+  --eth_rpc_url=http://ethereum-geth:8545
