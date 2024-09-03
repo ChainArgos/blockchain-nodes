58669079eab43c2baac059ef3eaff406115b3f9f
Alexey Zhokhov <alexey@zhokhov.com>
2024-09-03 20:55:33 +0200

    Fixed dogecoin package.

--- docker-dogecoin/build.sh
+++ docker-dogecoin/build.sh
@@ -4,7 +4,7 @@ cd "${ABSOLUTE_PATH}" || exit
 
 set -e
 
-export PACKAGE="bitcoin-core"
+export PACKAGE="dogecoin"
 export DOCKER_IMAGE_VERSION="latest"
 export DOCKER_REPOSITORY="donbeave/${PACKAGE}"
 
