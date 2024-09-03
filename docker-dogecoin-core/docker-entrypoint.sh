556d29919ab752bc3fcbf039d345b0b549478e4e
Alexey Zhokhov <alexey@zhokhov.com>
2024-09-04 00:24:31 +0200

    Dogecoin fixes

--- docker-dogecoin-core/docker-entrypoint.sh
+++ docker-dogecoin-core/docker-entrypoint.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 set -eou pipefail
 
-cat <<EOF >> /dogecoin.conf
-rpcuser=${RPCUSER:-chainargos}
-rpcpassword=${RPCPASSWORD:-`dd if=/dev/urandom bs=33 count=1 2>/dev/null | base64`}
-EOF
-
-mkdir -pv /data/litecoin-core
+mkdir -pv /data/dogecoin-core
 
 echo "Initialization completed successfully"
 
