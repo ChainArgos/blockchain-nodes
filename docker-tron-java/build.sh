d8f890e4831d8e73bab17bff2ce7972b3968e4ba
Alexey Zhokhov <alexey@zhokhov.com>
2024-09-03 23:54:05 +0200

    Rename java-tron to tron-java. For consistency with others.

--- docker-tron-java/build.sh
+++ docker-tron-java/build.sh
@@ -4,7 +4,7 @@ cd "${ABSOLUTE_PATH}" || exit
 
 set -e
 
-export PACKAGE="java-tron"
+export PACKAGE="tron-java"
 export DOCKER_IMAGE_VERSION="latest"
 export DOCKER_REPOSITORY="donbeave/${PACKAGE}"
 
@@ -26,20 +26,10 @@ docker buildx build ${DOCKER_BUILD_EXTRA_ARGS} \
   -t "${DOCKER_REPOSITORY}:${DOCKER_IMAGE_VERSION}-amd64" \
   -f Dockerfile.amd64 .
 
-echo "Building arm64"
-
-docker buildx build ${DOCKER_BUILD_EXTRA_ARGS} \
-  --push \
-  --provenance=false \
-  --platform linux/arm64 \
-  -t "${DOCKER_REPOSITORY}:${DOCKER_IMAGE_VERSION}-arm64" \
-  -f Dockerfile.arm64 .
-
 echo "Creating manifest"
 
 docker manifest create -a "${DOCKER_REPOSITORY}:${DOCKER_IMAGE_VERSION}" \
-  "${DOCKER_REPOSITORY}:${DOCKER_IMAGE_VERSION}-amd64" \
-  "${DOCKER_REPOSITORY}:${DOCKER_IMAGE_VERSION}-arm64"
+  "${DOCKER_REPOSITORY}:${DOCKER_IMAGE_VERSION}-amd64"
 
 echo "Pushing manifest"
 
