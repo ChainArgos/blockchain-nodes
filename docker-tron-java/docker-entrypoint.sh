abc0bd8f6f9d97883b569ed61e88719e78b6cd73
Alexey Zhokhov <alexey@zhokhov.com>
2024-09-03 23:57:09 +0200

    Rename /data/java-tron to automatically /data/tron-java.

--- docker-tron-java/docker-entrypoint.sh
+++ docker-tron-java/docker-entrypoint.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -eou pipefail
 
+if [ -d "/data/java-tron" ]; then
+  mv /data/java-tron /data/tron-java
+fi
+
 export JAVA_TOOL_OPTIONS="-XX:+UseConcMarkSweepGC -XX:+PrintGCDetails -Xloggc:./gc.log -XX:+PrintGCDateStamps -XX:+CMSParallelRemarkEnabled -XX:ReservedCodeCacheSize=256m -XX:+CMSScavengeBeforeRemark -Xms9g -Xmx24g -XX:MetaspaceSize=256m -XX:MaxMetaspaceSize=512m -XX:MaxDirectMemorySize=1G -XX:NewRatio=2 -XX:+ParallelRefProcEnabled -XX:+UseCMSInitiatingOccupancyOnly -XX:CMSInitiatingOccupancyFraction=70"
 
 echo "Initialization completed successfully"
