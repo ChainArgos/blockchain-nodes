b912b8b082b8792b2bd56b10cb6dc17e8ce3f3b7
Alexey Zhokhov <alexey@zhokhov.com>
2024-09-04 09:19:15 +0200

    Switch to use rpcauth for authentication (bitcoin, dogecoin, litecoin).

--- docker-bitcoin-core/docker-entrypoint.sh
+++ docker-bitcoin-core/docker-entrypoint.sh
@@ -2,8 +2,7 @@
 set -eou pipefail
 
 cat <<EOF >> /bitcoin.conf
-rpcuser=${RPCUSER:-chainargos}
-rpcpassword=${RPCPASSWORD:-`dd if=/dev/urandom bs=33 count=1 2>/dev/null | base64`}
+rpcauth=${RPCUSER:-chainargos}:${RPCPASSWORD:-f2977d600d1d3d978f930afad30ee691\$32f2a8b7c82b636789ce4c89932ecb55955c1b7ee8b7169cf4f6e355e6069e1f}
 EOF
 
 mkdir -pv /data/bitcoin-core
