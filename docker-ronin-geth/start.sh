ce27cd2973404ed13705da2023376a42cd4c6452
Alexey Zhokhov <alexey@zhokhov.com>
2024-09-18 14:17:13 +0200

    Simplified ronin config.

--- docker-ronin-geth/start.sh
+++ docker-ronin-geth/start.sh
@@ -3,19 +3,12 @@ set -eou pipefail
 
 exec ronin \
   --networkid=2020 \
-  --discovery.dns=enrtree://AIGOFYDZH6BGVVALVJLRPHSOYJ434MPFVVQFXJDXHW5ZYORPTGKUI@nodes.roninchain.com \
-  --finality.enable \
-  --txpool.globalqueue=10000 \
-  --txpool.globalslots=10000 \
-  --verbosity=3 \
+  --discovery.dns=enrtree://AIGOFYDZH6BGVVALVJLRPHSOYJ434MPFVVQFXJDXHW5ZYORPTGKUI@nodes.roninchain.com  \
   --syncmode=full \
   --datadir=/data/geth \
   --http \
-  --http.api eth,net,web3,consortium \
   --http.addr=0.0.0.0 \
   --http.corsdomain=* \
   --http.vhosts=* \
   --http.port=8745 \
-  --maxpeers=200 \
-  --cache=8000 \
-  --allow-insecure-unlock
+  --http.api=eth,net,web3,consortium
