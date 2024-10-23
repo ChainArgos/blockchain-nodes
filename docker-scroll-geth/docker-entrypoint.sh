badf852ec2e9d04e597b91da54385dd4ecddb889
Alexey Zhokhov <alexey@zhokhov.com>
2024-10-24 04:15:39 +0800

    Cleanup.

--- docker-scroll-geth/docker-entrypoint.sh
+++ docker-scroll-geth/docker-entrypoint.sh
@@ -4,13 +4,6 @@ set -eou pipefail
 # initialize the data directory
 ls -la /data > /dev/null
 
-if [ ! -f "/data/jwtsecret.hex" ]; then
-  echo "Generating JWT secret"
-
-  mkdir -pv /data
-  openssl rand -hex 32 | tr -d "\n" > /data/jwtsecret.hex
-fi
-
 echo "Initialization completed successfully"
 
 exec "$@"
