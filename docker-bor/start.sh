8e180d4f2b9c0c913bf0f16ceeef22cd97fe0f81
Alexey Zhokhov <alexey@zhokhov.com>
2024-11-04 01:25:14 +0800

    Configurable bor.heimdall option.

--- docker-bor/start.sh
+++ docker-bor/start.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -eou pipefail
 
+if [[ -z "${POLYGON_HEIMDALL_REST_SERVER_URL}" ]]; then
+  echo "ERROR: POLYGON_HEIMDALL_REST_SERVER_URL is not set"
+  exit 1
+fi
+
 exec bor server \
   -config "/config.toml" \
-  -bor.heimdall=http://polygon-heimdall-rest-server:1317
+  -bor.heimdall="${POLYGON_HEIMDALL_REST_SERVER_URL}"
