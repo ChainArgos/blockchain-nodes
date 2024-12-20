16fe51a686032846f8d44bee652539bd08a8d094
Alexey Zhokhov <alexey@zhokhov.com>
2024-12-20 22:46:48 +0300

    Removed polygon-heimdall-rest-server.

--- docker-heimdall/start.sh
+++ docker-heimdall/start.sh
@@ -1,27 +1,8 @@
 #!/bin/bash
 set -e
 
-if [[ -z "${CA_ETHEREUM_RPC_URL}" ]]; then
-  echo "ERROR: CA_ETHEREUM_RPC_URL is not set"
-  exit 1
-fi
-
-COMPONENT="${COMPONENT:-}"
-
-case $COMPONENT in
-
-  rest-server)
-    exec heimdalld rest-server \
-      --home=/data/heimdall \
-      --node "tcp://polygon-heimdall:26657"
-    ;;
-
-  *)
-    exec heimdalld start \
-      --chain=mainnet \
-      --home=/data/heimdall \
-      --home-client=/data/heimdallcli \
-      --rest-server
-    ;;
-
-esac
+exec heimdalld start \
+  --chain=mainnet \
+  --home=/data/heimdall \
+  --home-client=/data/heimdallcli \
+  --rest-server
