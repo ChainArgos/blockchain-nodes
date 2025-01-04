01fc3ad7e1eddbba511723e7b0aff4a16c0cb266
Alexey Zhokhov <alexey@zhokhov.com>
2025-01-05 01:34:37 +0300

    heimdall fixed node name

--- docker-heimdall/docker-entrypoint.sh
+++ docker-heimdall/docker-entrypoint.sh
@@ -9,16 +9,12 @@ if [ ! -d "/data/heimdall" ]; then
   heimdalld init --chain=mainnet --home=/data/heimdall --home-client=/data/heimdallcli
 
   # override with our config
-  cp /config.toml.sample /data/heimdall/config/config.toml
+  cp /config.toml /data/heimdall/config/config.toml
 
   # replace with mainnet genesis file
   cp /genesis.json /data/heimdall/config/genesis.json
 
   cp /heimdall-config.toml /data/heimdall/config/heimdall-config.toml
-
-  moniker=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 12; echo)
-
-  sed -i -e "s/NODE_NAME/$moniker/g" /data/heimdall/config/config.toml
 fi
 
 echo "Initialization completed successfully"
