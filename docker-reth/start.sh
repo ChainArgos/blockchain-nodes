be1c18df25ba809650156494b3bcc1b3c7406964
Alexey Zhokhov <alexey@zhokhov.com>
2025-02-10 01:40:29 +0300

    Fixed reth config.

--- docker-reth/start.sh
+++ docker-reth/start.sh
@@ -5,19 +5,21 @@ export EL_PEERS=$(grep '^enode://' "/config/el-peers.txt"| tr '\n' ',' | sed 's/
 
 exec reth \
   node \
-  --authrpc.jwtsecret=/data/jwtsecret.hex \
   --chain=/config/eth-genesis.json \
   --datadir=/data/reth \
+  --enable-discv5-discovery \
+  --identity=chainargos \
   --port=30308 \
   --http \
   --http.addr=0.0.0.0 \
   --http.port=8654 \
-  --http.corsdomain="*" \
+  --http.corsdomain=* \
   --bootnodes="$EL_PEERS" \
   --trusted-peers="$EL_PEERS" \
   --ws \
   --ws.addr=0.0.0.0 \
   --ws.port=8546 \
-  --ws.origins="*" \
+  --ws.origins=* \
   --authrpc.addr=0.0.0.0 \
-  --authrpc.port=8551
+  --authrpc.port=8551 \
+  --authrpc.jwtsecret=/data/jwtsecret.hex
