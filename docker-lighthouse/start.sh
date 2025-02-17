d5b8e839fd9a2227c3213b4ddceaa09eca2e3475
Alexey Zhokhov <alexey@zhokhov.com>
2025-02-18 01:30:45 +0500

    CA_ETHEREUM_EXECUTION_URL env.

--- docker-lighthouse/start.sh
+++ docker-lighthouse/start.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 set -eou pipefail
 
+if [[ -z "${CA_ETHEREUM_EXECUTION_URL}" ]]; then
+  echo "ERROR: CA_ETHEREUM_EXECUTION_URL is not set"
+  exit 1
+fi
+
 # wait until local execution client comes up (authed so will return 401 without token)
-until [ "$(curl -s -w '%{http_code}' -o /dev/null "http://ethereum-geth:8551")" -eq 401 ]; do
+until [ "$(curl -s -w '%{http_code}' -o /dev/null "${CA_ETHEREUM_EXECUTION_URL}")" -eq 401 ]; do
   echo "waiting for execution (geth) client to be ready"
   sleep 5
 done
@@ -11,7 +16,7 @@ exec lighthouse \
   --datadir=/data/lighthouse \
   --network=mainnet \
   beacon_node \
-  --execution-endpoint=http://ethereum-geth:8551 \
+  --execution-endpoint="${CA_ETHEREUM_EXECUTION_URL}" \
   --execution-jwt=/data/jwtsecret.hex \
   --checkpoint-sync-url=https://beaconstate.info \
   --http \
