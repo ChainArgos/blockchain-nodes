14de37843187c3af86285d4a5ab12a053e3f235a
Alexey Zhokhov <alexey@zhokhov.com>
2025-02-18 20:53:33 +0500

    Force cleanup scripts.

---
+++ clean-base-op-geth.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+ABSOLUTE_PATH=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+cd "${ABSOLUTE_PATH}" || exit
+
+docker stop -s 9 optimism-op-node
+docker rm optimism-op-node
+
+docker stop -s 9 optimism-op-geth
+docker rm optimism-op-geth
+
+rm -rf /blockchain/disk1/optimism/
+ls -la /blockchain/disk1
