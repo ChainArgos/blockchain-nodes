b561c6f3ae46b07da15b0e549046515f50ba0e22
Alexey Zhokhov <alexey@zhokhov.com>
2025-02-21 03:19:32 +0500

    fixed bor port and enabled nat

--- docker-bor/start.sh
+++ docker-bor/start.sh
@@ -1,10 +1,40 @@
 #!/bin/bash
 set -eou pipefail
 
+get_public_ip() {
+  # Define a list of HTTP-based providers
+  local PROVIDERS=(
+    "http://ifconfig.me"
+    "http://api.ipify.org"
+    "http://ipecho.net/plain"
+    "http://v4.ident.me"
+  )
+  # Iterate through the providers until an IP is found or the list is exhausted
+  for provider in "${PROVIDERS[@]}"; do
+    local IP
+    IP=$(curl -s "$provider")
+    # Check if IP contains a valid format (simple regex for an IPv4 address)
+    if [[ $IP =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+      echo "$IP"
+      return 0
+    fi
+  done
+  return 1
+}
+
+# public-facing P2P node, advertise public IP address
+if PUBLIC_IP=$(get_public_ip); then
+  echo "Fetched public IP is: $PUBLIC_IP"
+else
+  echo "ERROR: Could not retrieve public IP."
+  exit 1
+fi
+
 until [ "$(curl -s http://polygon-heimdall:26657/status | jq '.result.sync_info.catching_up')" = "false" ]; do
   echo "waiting for heimdall client to be ready"
   sleep 5
 done
 
 exec bor server \
-  -config "/config.toml"
+  -config "/config.toml" \
+  -nat=extip:"$PUBLIC_IP"
