6a6e25ea5020e93a64f3a8951b5f769d4aef3582
Alexey Zhokhov <alexey@zhokhov.com>
2025-06-17 01:15:17 +0300

    Improved arbitrum docker image.

--- docker-arbitrum/start.sh
+++ docker-arbitrum/start.sh
@@ -23,4 +23,4 @@ exec nitro \
   --http.vhosts=* \
   --execution.caching.archive \
   --execution.tx-lookup-limit=0 \
-  --validation.wasm.allowed-wasm-module-roots=/workspace/target/machines
+  --validation.wasm.allowed-wasm-module-roots=/workspace/nitro-legacy/machines,/workspace/target/machines
