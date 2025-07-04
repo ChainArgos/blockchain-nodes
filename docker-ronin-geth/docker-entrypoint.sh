ff140fdb2f338f61c5a34f7c3c9bcf079a77cf9f
Alexey Zhokhov <alexey@zhokhov.com>
2025-07-04 14:06:39 +0300

    Attempt to re-init ronin with a new genesis config.

--- docker-ronin-geth/docker-entrypoint.sh
+++ docker-ronin-geth/docker-entrypoint.sh
@@ -7,11 +7,9 @@ set -eo pipefail
 
 eval "$(mise activate bash)"
 
-if [ ! -d "/data/geth" ]; then
-  echo "Init genesis geth."
+echo "Init genesis geth."
 
-  ronin --db.engine=pebble --state.scheme=path --datadir=/data/geth init /config/mainnet.json
-fi
+ronin --db.engine=pebble --state.scheme=path --datadir=/data/geth init /config/mainnet.json
 
 echo "Initialization completed successfully"
 
